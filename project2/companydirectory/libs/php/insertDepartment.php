361203797f9bea985bda5efaf7b5d3d09c64d95a
Mo Hirsi <129159947+peahenn@users.noreply.github.com>
2023-12-12 16:05:47 +0000

    Add files via upload

---
+++ project2/companydirectory/libs/php/insertDepartment.php
@@ -0,0 +1,34 @@
+<?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+$executionStartTime = microtime(true);
+include("config.php");
+header('Content-Type: application/json; charset=UTF-8');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $updatedDepartmentName = $_POST['name'];
+
+    $conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
+    $query = $conn->prepare('INSERT INTO department (name) VALUES (?)');
+    $query->bind_param("s", $updatedDepartmentName);
+
+    if ($query->execute()) {
+        $output['status']['code'] = "200";
+        $output['status']['name'] = "ok";
+        $output['status']['description'] = "Personnel updated successfully";
+    } else {
+        $output['status']['code'] = "400";
+        $output['status']['name'] = "error";
+        $output['status']['description'] = "Error updating personnel: " . $conn->error;
+    }
+    mysqli_close($conn);
+    echo json_encode($output);
+} else {
+    $output['status']['code'] = "405";
+    $output['status']['name'] = "method not allowed";
+    $output['status']['description'] = "Method not allowed";
+    mysqli_close($conn);
+    echo json_encode($output);
+}
+?>
